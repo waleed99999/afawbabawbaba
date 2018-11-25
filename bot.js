fee4c849138bebee9ed962174e6484af8c4dba94
waleed99999 <45340741+waleed99999@users.noreply.github.com>
2018-11-25 22:23:23 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -180,6 +180,21 @@ if (message.content === '!spam') {
         }
       }
 });
+var prefix="$"
+//say
+client.on('message', message => {
+  if (message.author.bot) return;
+  if (!message.content.startsWith(prefix)) return;
+
+  let command = message.content.split(" ")[0];
+  command = command.slice(prefix.length);
+
+  let args = message.content.split(" ").slice(1);
+
+  if (command == "say") {
+   message.channel.sendMessage(args.join("  "))
+   message.delete()
+  }
 
 
 client.login(process.env.TOKEN);// لا تغير فيها شيء
